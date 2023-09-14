3052f3ca425460d4131b7dd2418bc0a1c45dd979
reza <reza.pratama@concretejakarta.com>
2023-09-14 18:54:15 +0700

    create page edit

--- resources/js/Pages/Backoffice/Klasemen/Edit.jsx
+++ resources/js/Pages/Backoffice/Klasemen/Edit.jsx
@@ -1,7 +1,7 @@
 import Header from "@/Components/Backoffice/Header";
 import Backend from "@/Layouts/Backoffice/Backend";
 import { usePage } from "@inertiajs/react";
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Button, IconButton, TextField, Tooltip, Typography } from "@mui/material";
 import React from "react";
 
 const Edit = ({ klasemen }) => {
@@ -10,51 +10,20 @@ const Edit = ({ klasemen }) => {
 
     return (
         <Box sx={{ m: "20px" }}>
-            <Header
-                title={`Welcome Back ${user.name}`}
-                subtitle={`Your Role ${user.role.map(
-                    (role, index) => role.name
-                )}`}
-            ></Header>
+            <Header title={`Welcome Back ${user.name}`} subtitle={`Your Role ${user.role.map((role, index) => role.name)}`}></Header>
 
-            <Typography variant="h2" sx={{ fontSize: 25 }} gutterBottom>
-                Klasemen Edit
-            </Typography>
+            <Typography variant="h2" sx={{ fontSize: 25 }} gutterBottom>Klasemen Edit</Typography>
 
-            <Box>
-                <TextField
-                    id="outlined-required"
-                    label="Title"
-                    defaultValue={klasemen.title}
-                    color="success"
-                    required
-                    fullWidth
-                />
-                <TextField
-                    id="outlined-required"
-                    label="Score"
-                    defaultValue={klasemen.score}
-                    color="success"
-                    required
-                    fullWidth
-                />
-                <TextField
-                    id="outlined-required"
-                    label="CTA Title"
-                    defaultValue={klasemen.cta_title}
-                    color="success"
-                    required
-                    fullWidth
-                />
-                <TextField
-                    id="outlined-required"
-                    label="CTA Link"
-                    defaultValue={klasemen.cta_link == null ? "Null" : ""}
-                    color="success"
-                    required
-                    fullWidth
-                />
+            <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px' }} container>
+                <TextField id="outlined-required" label="Title" defaultValue={klasemen.title} color="success" required fullWidth />
+                <TextField id="outlined-required" label="Score" defaultValue={klasemen.score} color="success" required fullWidth />
+                <TextField id="outlined-required" label="CTA Title" defaultValue={klasemen.cta_title} color="success" required fullWidth />
+                <TextField id="outlined-required" label="CTA Link" defaultValue={klasemen.cta_link == null ? "Null" : ""} color="success" required fullWidth />
             </Box>
+
+            <Tooltip title="Save">
+                <Button variant="contained" sx={{ backgroundColor: 'blue', marginTop: '20px', float: 'right'}}>Save</Button>
+            </Tooltip>
         </Box>
     );
 };
