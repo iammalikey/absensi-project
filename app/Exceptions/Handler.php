e9d7c5c14bd3b614a9929309b063610f6efc813a
firmanmimang <fhidayat131@gmail.com>
2023-09-20 12:18:09 +0700

    fix exception handler errorPage inertia

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,8 +38,8 @@ class Handler extends ExceptionHandler
     {
         $response = parent::render($request, $e);
 
-        // if (!app()->environment(['local', 'staging']) && in_array($response->status(), [500, 503, 404, 403])) {
-        if (in_array($response->status(), [500, 503, 404, 403])) {
+        if (!app()->environment(['local', 'staging']) && in_array($response->status(), [500, 503, 404, 403])) {
+        // if (in_array($response->status(), [500, 503, 404, 403])) {
             return inertia('Error/ErrorPage', ['status' => $response->status()])
                 ->toResponse($request)
                 ->setStatusCode($response->status());
